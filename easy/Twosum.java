7ff7d9e413961ca9aeb0d69a85c991a3b2267e5d
Ranjith Kumar. S <75241909+RanjithKumar-droid@users.noreply.github.com>
2021-04-14 13:19:44 +0530

    added main method

--- easy/Twosum.java
+++ easy/Twosum.java
@@ -1,5 +1,5 @@
 class Twosum {
-    public int[] twoSum(int[] nums, int target) {
+   public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
         int diff = target - nums[i];
@@ -9,5 +9,17 @@ class Twosum {
         map.put(nums[i], i);
     }
     throw new IllegalArgumentException("No two sum solution");
+   }
+  public static void main(String args[]) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter len of array");
+	int len = scan.nextInt();
+	int arr[] = new int[len];
+	System.out.println("Enter "+len+" no of inputs");
+	for(int i=0;i<len;i++) {
+		int arr[i] = scan.nextInt();
+	}
+	int tar = scan.nextLine();
+	sum(arr, tar);
+  }
 }
-    }
