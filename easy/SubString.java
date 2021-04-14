3c0d067bf5c25bfa4e4d92c7197f96ddfd41b3bf
Ranjith Kumar. S <75241909+RanjithKumar-droid@users.noreply.github.com>
2021-04-14 13:28:53 +0530

    added main method

--- easy/SubString.java
+++ easy/SubString.java
@@ -6,16 +6,22 @@ Class SubString {
 	
 		HashSet<Character> hash_set = new HashSet();
 
-		while(b_pointer < s.length()) {
+	     while(b_pointer < s.length()) {
 			if(!hash_set.contains(s.charAt(b_pointer))) {
 			    hash_set.add(s.charAt(b_pointer));
 			    b_pointer++;
 			    max = Math.max(hash_set.size(), max);
-}
-else {
-	hash_set.remove(s.charAt(a_pointer));
-	a_pointer++; 
-}
-}
-}
+	}
+		else {
+			hash_set.remove(s.charAt(a_pointer));
+			a_pointer++; 
+		}
+	      }
+			}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String inp = sc.nextLine();
+		substr(inp);	
+	}
 }
