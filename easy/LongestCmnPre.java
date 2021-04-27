e01bace51f3cc775217022a73eb9b17875dbf003
Ranjith Kumar. S <75241909+RanjithKumar-droid@users.noreply.github.com>
2021-04-27 13:29:57 +0530

    added main method

--- easy/LongestCmnPre.java
+++ easy/LongestCmnPre.java
@@ -10,4 +10,16 @@ class LongesCmnPre{
     }
     return strs[0];
     }
+	public static void main(String args[]) {
+	    Scanner sc = new Scanner(System.in);
+	    Scanner st = new Scanner(System.in);
+	    System.out.println("Enter len of array");
+	    int len = sc.nextInt();
+	    String arr[] = new String[len];
+	    System.out.println("Enter str array of "+len+" inputs");
+	    for(int i : arr){
+		arr[i] = st.nextLine();
+	    }
+	    System.out.println(longestCommonPrefix(arr));
+	}
 }    
