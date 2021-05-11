20156255ffa39cd970c4013e6bfefafbd310649f
Ranjith Kumar. S <75241909+RanjithKumar-droid@users.noreply.github.com>
2021-05-11 18:33:04 +0530

    Add files via upload

---
+++ 2021-05-11/LevelOrder.java
@@ -0,0 +1,51 @@
+package binarytree;
+
+import java.util.*;
+
+public class LevelOrder {
+	class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int val){
+			this.data = val;
+		}
+	}
+
+	
+	public List<List<Integer>> levelorder(Node root){
+		
+		List<List<Integer>> result = new ArrayList<>();
+		
+		if(root == null) return null;
+		
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while(!queue.isEmpty()) {
+			
+			 int levelsize = queue.size();
+           List<Integer> level = new ArrayList<>();
+          
+           for(int i=0; i<levelsize; i++){
+               Node node = queue.poll();
+               level.add(node.data);
+              
+               if(node.left != null){
+                   queue.offer(node.left);
+               }
+               
+               if(node.right != null){
+                   queue.offer(node.right);
+               }
+           }
+           result.add(level);
+       }
+		return result;
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
