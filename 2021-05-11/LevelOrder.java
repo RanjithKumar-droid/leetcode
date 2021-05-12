e6a815b0b4df2b037594b08ea7ef05bba217779d
Ranjith Kumar. S <75241909+RanjithKumar-droid@users.noreply.github.com>
2021-05-12 09:32:01 +0530

    Update LevelOrder.java

--- 2021-05-11/LevelOrder.java
+++ 2021-05-11/LevelOrder.java
@@ -13,6 +13,37 @@ public class LevelOrder {
 		}
 	}
 
+	public List<List<Integer>> levelorder(Node root){
+		
+		List<List<Integer>> result = new ArrayList<>();
+		
+		if(root == null) return null;
+		
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while(!queue.isEmpty()) {
+			
+			 int levelsize = queue.size();
+           List<Integer> level = new ArrayList<>();
+          
+           for(int i=0; i<levelsize; i++){
+               Node node = queue.poll();
+               level.add(node.data);
+              
+               if(node.left != null){
+                   queue.offer(node.left);
+               }
+               
+               if(node.right != null){
+                   queue.offer(node.right);
+               }
+           }
+           result.add(level);
+       }
+		return result;
+		
+	}
 	
 	public List<List<Integer>> levelorder(Node root){
 		
@@ -46,6 +77,8 @@ public class LevelOrder {
 		
 	}
 	public static void main(String[] args) {
-		
+		LevelOrder obj = new LevelOrder();
+		Node root = obj.createtree();
+		obj.levelorder(root);
 	}
 }
